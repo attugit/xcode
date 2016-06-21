b0a02ffc7392fd2254dc0f35ad6fc40136ef0b45
Karol Wozniak <karol.wozniak@email.com>
2016-06-21 20:09:29 +0200

    emplace and erase

--- src/lib/tree.hpp
+++ src/lib/tree.hpp
@@ -34,7 +34,22 @@ namespace xcode
 
   template <typename T, typename Alloc = std::allocator<T>>
   struct siblings {
+    using allocator_type = Alloc;
+
   private:
+    using traits = std::allocator_traits<allocator_type>;
+
+  public:
+    using size_type = typename traits::size_type;
+    using difference_type = typename traits::difference_type;
+    using value_type = typename traits::value_type;
+    using pointer = typename traits::pointer;
+    using const_pointer = typename traits::const_pointer;
+    using reference = value_type&;
+    using const_reference = value_type const&;
+
+  private:
+    using node_allocator = typename traits::template rebind_alloc<node<T>>;
     template <typename ValueType, typename NodeType, typename LinkType>
     struct iterator_impl : boost::iterator_facade<iterator_impl<ValueType, NodeType, LinkType>,
                                                   ValueType,
@@ -66,6 +81,8 @@ namespace xcode
       }
 
     private:
+      template <typename, typename>
+      friend struct siblings;
       friend class boost::iterator_core_access;
       template <typename, typename, typename>
       friend struct iterator_impl;
@@ -82,8 +99,48 @@ namespace xcode
       link_type* link = nullptr;
     };
 
+    struct impl_type : node_allocator {
+      node_links root;
+      size_type size = 0;
+    } impl;
+
   public:
-    using iterator = iterator_impl<T, node<T>, node_links>;
-    using const_iterator = iterator_impl<const T, const node<T>, const node_links>;
+    using iterator = iterator_impl<value_type, node<value_type>, node_links>;
+    using const_iterator =
+        iterator_impl<const value_type, const node<value_type>, const node_links>;
+    using reverse_iterator = std::reverse_iterator<iterator>;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+
+    siblings() { impl.root.hook_after(impl.root); }
+    ~siblings()
+    {
+      while (!empty()) {
+        erase(begin());
+      }
+    }
+    iterator begin() { return iterator{impl.root.next}; }
+    const_iterator cbegin() const { return const_iterator{impl.root.next}; }
+    const_iterator begin() const { return cbegin(); }
+    iterator end() { return iterator{&impl.root}; }
+    const_iterator cend() const { return const_iterator{&impl.root}; }
+    const_iterator end() const { return cend(); }
+    bool empty() const { return begin() == end(); }
+    size_type size() const { return impl.size; }
+    template <typename... Args>
+    void emplace(iterator it, Args&&... args)
+    {
+      auto ptr = impl.allocate(1);
+      new (ptr) node<T>(std::forward<Args>(args)...);
+      ptr->hook_after(*(it.link));
+      ++impl.size;
+    }
+    void erase(iterator it)
+    {
+      auto ptr = static_cast<node<T>*>(it.link);
+      ptr->unlink();
+      --impl.size;
+      ptr->~node<T>();
+      impl.deallocate(ptr, 1);
+    }
   };
 }
--- test/tree/tree_test.cpp
+++ test/tree/tree_test.cpp
@@ -141,4 +141,29 @@ namespace
     siblings<int>::const_iterator cit(&n);
     EXPECT_EQ(*cit, 7);
   }
+
+  struct siblings_test : ::testing::Test {
+  };
+
+  TEST(siblings_test, canDefaultConstruct)
+  {
+    siblings<int> sib;
+    EXPECT_TRUE(sib.empty());
+    EXPECT_EQ(sib.size(), 0u);
+    EXPECT_EQ(sib.begin(), sib.end());
+  }
+
+  TEST(siblings_test, canEmplace)
+  {
+    siblings<int> sib;
+    sib.emplace(sib.end(), 7);
+    EXPECT_FALSE(sib.empty());
+    EXPECT_EQ(sib.size(), 1u);
+    EXPECT_NE(sib.begin(), sib.end());
+    EXPECT_EQ(*sib.begin(), 7);
+    sib.erase(sib.begin());
+    EXPECT_TRUE(sib.empty());
+    EXPECT_EQ(sib.begin(), sib.end());
+    EXPECT_EQ(sib.size(), 0u);
+  }
 }
